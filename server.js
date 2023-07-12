74379ccc713952ce73d1489db8f3b580ebebaa37
yoricky42 <yorickyves42@gmail.com>
2023-07-12 20:44:37 +0000

    d

--- server.js
+++ server.js
@@ -14,130 +14,193 @@ app.use(cors());
 // const URL = "http://localhost:5500/";
 
 app.get("/pdf-generator", async (req, res) => {
-  const reportFormat = [
+  const reportFormat_id = [
     {
       format: "CarteID",
       params: ["nom", "prenom", "date_naiss"],
+      host: null,
+      ip_host: "80.240.29.46",
     },
     {
       format: "hoballa_report",
       params: ["id_c"],
+      host: null,
+      ip_host: "80.240.29.46",
     },
     {
       format: "ticket_caisse",
       params: ["id_t"],
+      host: null,
+      ip_host: "80.240.29.46",
     },
     {
       format: "Font1",
       params: ["contrat_id"],
-      host:"hoballahome",
-      ip_host:"80.240.29.46"
+      host: "hoballahome",
+      ip_host: "80.240.29.46",
     },
   ];
 
   let makeRequestForPDF = true;
-  let reqToSendIs = ""
-  let reqToken = ""
-
-  reportFormat.forEach((el)=>{
-    if(el.host === req.query["host"]){
-      reqToSendIs = `http://${el.ip_host}:8080/jasperserver/rest_v2/reports/reports/`;
-      reqToken = `http://${el.ip_host}:8080/jasperserver/rest_v2/login?j_username=jasperadmin&j_password=jasperadmin`
-    }
-  })
-
+  let reqToSendIs = "";
+  let reqToken = "";
   let error =
-    "ERREUR PARAMETRE POUR " +
-    req.query.reportFormat +
-    " FORMAT: Parametre manquant ==>";
+    "Erreur :  paramètre pour le format id= " +
+    req.query.reportFormat_id.toString().toUpperCase() +
+    " - Parametre manquant ==>";
 
-  reportFormat.forEach((el) => {
-    if (el.format === req.query["reportFormat"]) {
-      reqToSendIs = reqToSendIs + req.query["reportFormat"] + ".html?";
-      for (item in el.params) {
-        if (
-          req.query[el.params[item]] === "" ||
-          req.query[el.params[item]] === undefined
-        ) {
-          makeRequestForPDF = false;
-          error = error + " | " + el.params[item];
-        }
-        reqToSendIs =
-          item >= 1
-            ? reqToSendIs +
-              "&" +
-              el.params[item] +
-              "=" +
-              req.query[el.params[item]]
-            : reqToSendIs + el.params[item] + "=" + req.query[el.params[item]];
-      }
-    }
-  });
-
-  const formatExist = reportFormat.find(
-    (el) => el.format === req.query["reportFormat"]
+  const hostExist = reportFormat_id.find((el) => el.host === req.query["host"]);
+  const formatExist = [hostExist].find(
+    (el) => el.format === req.query["reportFormat_id"]
   );
 
-  const hostExist = reportFormat.find(
-    (el) => el.host === req.query["host"]
-  );
-
-  if (makeRequestForPDF && formatExist && hostExist) {
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    const page = await browser.newPage();
-    await page.setViewport({
-      width: 1440,
-      height: 1080,
-      deviceScaleFactor: 1,
-    });
-
-    await page
-      .goto(
-        reqToken,
-        { waitUntil: "networkidle0" }
-      )
-      .then(() => page.goto(reqToSendIs, { waitUntil: "networkidle0" }));
-
-    await page.emulateMediaType("print");
-    await page.addStyleTag({
-      content: `body{ 
-                    margin:0;
-                    padding:0;
-                  }`,
-    });
-
-    const ItemToGetInPDF = await page.$(".jrPage>tbody");
-    const rectItemToGetInPDF = await page.evaluate((info) => {
-      const { top, left, bottom, right, height, width } =
-        info.getBoundingClientRect();
-      return { top, left, bottom, right, height, width };
-    }, ItemToGetInPDF);
-
-    await page
-      .pdf({
-        path: `pdf.pdf`,
-        width: rectItemToGetInPDF.width,
-        height: rectItemToGetInPDF.height,
-        margin: 0,
-        printBackground: true,
-      })
-      .then(async (pdf) => {
-        res.sendFile(__dirname + "/pdf.pdf");
-        await browser.close();
-      });
-  } else {
+  if (!hostExist) {
     res.send(
-      hostExist ? 'Params "host" on url is missing ' : 
-      formatExist ? error : 
-      `Server listening on Port : ${port} | IP address : ${IP} | ERROR : format ` +req.query["reportFormat"]
+      `<body>
+          <style>
+          h1 {
+            color: #26b72b;
+          }
+          h3 {
+            color: red;
+          }
+          </style>
+          <h1 >Server listening on Port : ${port} | IP address : ${IP}</h1> 
+          <h3 >
+          ${
+            req.query["host"] === undefined || req.query["host"] === ""
+              ? "Le paramètre 'hôte' est manquant ou vide dans l'url"
+              : `Erreur : l'hôte '${req.query["host"]}' n'est pas valide`
+          }
+          </h3> 
+
+          </body>
+          `
     );
+  } else if (!formatExist) {
+    res.send(`
+    <body>
+          <style>
+          h1 {
+            color: #26b72b;
+          }
+          h3 {
+            color: red;
+          }
+          </style>
+          <h1 >Server listening on Port : ${port} | IP address : ${IP}</h1> 
+          <h3 >
+          ${
+            req.query["reportFormat_id"] === undefined || req.query["reportFormat_id"] === ""
+              ? "Le paramètre 'reportFormat_id' est manquant ou vide dans l'url"
+              : `Erreur : reportFormat_id '${req.query["reportFormat_id"]}' n'existe pas sur l'hôte '${hostExist.host}'`
+          }
+          </h3> 
+
+          </body>
+          `);
+  } else if (formatExist) {
+    // Dans le cas ou on a l'hote et les
+    reqToSendIs = `http://${hostExist?.ip_host}:8080/jasperserver/rest_v2/reports/reports/`;
+    reportFormat_id.forEach((el) => {
+      if (el.format === req.query["reportFormat_id"]) {
+        reqToSendIs = reqToSendIs + req.query["reportFormat_id"] + ".html?";
+        for (item in el.params) {
+          if (
+            req.query[el.params[item]] === "" ||
+            req.query[el.params[item]] === undefined
+          ) {
+            makeRequestForPDF = false;
+            error = error + " | " + el.params[item];
+          }
+          reqToSendIs =
+            item >= 1
+              ? reqToSendIs +
+                "&" +
+                el.params[item] +
+                "=" +
+                req.query[el.params[item]]
+              : reqToSendIs +
+                el.params[item] +
+                "=" +
+                req.query[el.params[item]];
+        }
+      }
+    });
   }
+  if (makeRequestForPDF) {
+    reqToken = `http://${hostExist?.ip_host}:8080/jasperserver/rest_v2/login?j_username=jasperadmin&j_password=jasperadmin`;
+
+    if (makeRequestForPDF && formatExist && hostExist) {
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+      const page = await browser.newPage();
+      await page.setViewport({
+        width: 1440,
+        height: 1080,
+        deviceScaleFactor: 1,
+      });
+  
+      await page
+        .goto(
+          reqToken,
+          { waitUntil: "networkidle0" }
+        )
+        .then(() => page.goto(reqToSendIs, { waitUntil: "networkidle0" }));
+  
+      await page.emulateMediaType("print");
+      await page.addStyleTag({
+        content: `body{
+                      margin:0;
+                      padding:0;
+                    }`,
+      });
+  
+      const ItemToGetInPDF = await page.$(".jrPage>tbody");
+      const rectItemToGetInPDF = await page.evaluate((info) => {
+        const { top, left, bottom, right, height, width } =
+          info.getBoundingClientRect();
+        return { top, left, bottom, right, height, width };
+      }, ItemToGetInPDF);
+  
+      await page
+        .pdf({
+          path: `pdf.pdf`,
+          width: rectItemToGetInPDF.width,
+          height: rectItemToGetInPDF.height,
+          margin: 0,
+          printBackground: true,
+        })
+        .then(async (pdf) => {
+          res.sendFile(__dirname + "/pdf.pdf");
+          await browser.close();
+        });
+    }
+  } else {
+    res.send(`
+    <body>
+          <style>
+          h1 {
+            color: #26b72b;
+          }
+          h3 {
+            color: red;
+          }
+          </style>
+          <h1 >Server listening on Port : ${port} | IP address : ${IP}</h1> 
+          <h3 >
+          ${
+            error
+          }
+          </h3> 
+
+          </body>
+          `);
+  }
+
 });
 
 app.listen(process.env.PORT || port, async () => {
-  console.log(
-    `Server listening on Port : ${port} | IP address : ${IP}`
-  );
+  console.log(`Server listening on Port : ${port} | IP address : ${IP}`);
 });
