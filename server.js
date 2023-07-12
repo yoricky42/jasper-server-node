19dc8beb5b9d83d2d74005280bb8c3c4f450d4a2
yoricky42 <yorickyves42@gmail.com>
2023-07-12 12:37:08 +0000

    f

--- server.js
+++ server.js
@@ -110,7 +110,7 @@ app.get("/pdf-generator", async (req, res) => {
     res.send(
       formatExist
         ? error
-        : "Ce type de format n'existe pas : " + req.query["reportFormat"]
+        : `Server listening on Port : ${port} | IP address : ${IP.address()} | ERROR : format inexistant` + req.query["reportFormat"]
     );
   }
 });
